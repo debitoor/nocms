c773e36b8225aa1fabf15b88805c703076ca5dc9
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-01-19 20:16:44 +0100

    removes built in image resource plugin

--- src/lib/plugins/nocms-plugin-image-resources/index.js
+++
@@ -1,7 +0,0 @@
-import createImageResourceProvider from './createImageResourceProvider';
-
-export function activate ({createResourceProvider, registerResourceProvider, findFiles, readFile, watchFiles, writeFile}) {
-	const imageResourceProvider = createImageResourceProvider({findFiles, readFile, watchFiles, writeFile});
-
-	registerResourceProvider(imageResourceProvider);
-}
