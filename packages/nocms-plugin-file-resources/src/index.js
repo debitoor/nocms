4d497379fbee5a37c2dad9eaa25087763481a5c8
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-01-22 21:17:28 +0100

    initial commit

---
+++ packages/nocms-plugin-file-resources/src/index.js
@@ -0,0 +1,11 @@
+import createFileResourceProvider from './createFileResourceProvider';
+
+export function activate ({registerResourceProvider, findFiles, readFile, watchFiles, writeFile}) {
+	const fileResourceProvider = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, './', '**/!(_)*.?(docx|gif|jpeg|jpg|pdf|ico|png|svg|txt|xlsx)');
+	const sharedFileResourceProvider = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, '../../', 'shared/**/!(_)*.?(docx|gif|jpeg|jpg|pdf|ico|png|svg|txt|xlsx)');
+	const sharedFaviconFileResourceProviderc = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, '../../shared/favicon', '*');
+
+	registerResourceProvider(fileResourceProvider);
+	registerResourceProvider(sharedFileResourceProvider);
+	registerResourceProvider(sharedFaviconFileResourceProviderc);
+}
