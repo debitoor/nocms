60a785b035d5fc5c494eb4a923fd08697b5d2bb7
Daniel Hillmann <hiller@live.dk>
2017-07-06 15:00:36 +0200

    Start making module load config file with path and glob

---
+++ packages/nocms-plugin-file-resources/src/getConfigurationFile.js
@@ -0,0 +1,10 @@
+
+const nocmsrc_filename = '.nocmsrc';
+
+export default function getConfigFile () {
+
+	return {
+		path: '',
+		glob: ''
+	};
+}
--- packages/nocms-plugin-file-resources/src/index.js
+++ packages/nocms-plugin-file-resources/src/index.js
@@ -1,8 +1,12 @@
 import createFileResourceProvider from './createFileResourceProvider';
+import configFile from './getConfigurationFile';
 
 export function activate ({registerResourceProvider, findFiles, readFile, watchFiles, writeFile}) {
+	const configPath = configFile.directory;
+	const configGlob = configFile.glob;
+
 	const fileResourceProvider = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, './', '**/!(_)*.?(docx|gif|jpeg|jpg|pdf|ico|png|svg|txt|xlsx)');
-	const sharedFileResourceProvider = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, '../../', 'shared/**/!(_)*.?(docx|gif|jpeg|jpg|pdf|ico|png|svg|txt|xlsx)');
+	const sharedFileResourceProvider = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, configPath, configGlob);
 	const sharedFaviconFileResourceProviderc = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, '../../shared/favicon', '*');
 
 	registerResourceProvider(fileResourceProvider);
