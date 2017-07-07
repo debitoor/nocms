0f287c5a85e566d92f69d818a047d3928c384a9a
Daniel Hillmann <hiller@live.dk>
2017-07-07 09:11:45 +0200

    Small change

--- packages/nocms-plugin-file-resources/src/index.js
+++ packages/nocms-plugin-file-resources/src/index.js
@@ -2,7 +2,7 @@ import createFileResourceProvider from './createFileResourceProvider';
 import configFile from './getConfigurationFile';
 
 export function activate ({registerResourceProvider, findFiles, readFile, watchFiles, writeFile}) {
-	const configPath = configFile.directory;
+	const configPath = configFile.path;
 	const configGlob = configFile.glob;
 
 	const fileResourceProvider = createFileResourceProvider(findFiles, readFile, watchFiles, writeFile, './', '**/!(_)*.?(docx|gif|jpeg|jpg|pdf|ico|png|svg|txt|xlsx)');
